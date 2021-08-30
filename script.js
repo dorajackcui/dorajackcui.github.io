443246d24483cacb59a54b04515b0c856bedcb93
dorajackcui <dorajackcui@gmail.com>
2021-08-30 18:40:09 +0200

    add project page

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function drawLine(context, x1, y1, x2, y2) {
   context.beginPath();
-  context.strokeStyle = 'blue';
+  context.strokeStyle = '#c5c0b2';
   context.lineWidth = 1;
   context.moveTo(x1, y1);
   context.lineTo(x2, y2);
